fe8dab5b0b11652f6a4179ef8000e9b49cca228a
Maurim90 <mera.mauricio13@gmail.com>
2025-05-12 14:51:46 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer: React.FC = () => {
                 <Phone size={24} />
               </a>
               <a 
-                href="mailto:daro-umerez@hotmail.com" 
+                href="mailto:mera.mauricio13@gmail.com" 
                 className="text-gray-400 hover:text-pink-400 transition-colors"
               >
                 <Mail size={24} />
